442e1264018d8eaa3c1e349c257d848ef0eecb40
Sandhy Kurniawan <sipowl195@gmail.com>
2018-10-14 21:54:05 +0700

    Initial commit

---
+++ CTVM2App/MyHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace CTVM2App
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void Announce(string message)
+        {
+            //broadcast
+            Clients.All.Announce(message);
+        }
+
+        public void Send(string shortmessage)
+        {
+            Clients.All.SendChat(shortmessage);
+        }
+
+        public DateTime GetServerDateTime()
+        {
+            return DateTime.Now;
+            //Clients.Caller.DisplayDateTime();
+        }
+    }
+}
---
+++ CTVM2App/Scripts/SignalR.js
@@ -0,0 +1,49 @@
+﻿//alert("Hey! its awesome!");
+(function () {
+    //var myHub = $.connection.myHub; //connect Hub(Class) - camelCase
+    var myHub = $.connection.chat; //connect Hub(Class) - Attribute
+    
+    //event click Date
+    $("#click-me").on("click", function () {
+        myHub.server.getServerDateTime()
+        .done(function (data) {
+            WriteToPage(data);
+        })
+        .fail(function (e) {
+            WriteToPage(e);
+        })
+    })
+    $("#click-fromsendchat").click(function () {
+        var textmessage = $("#txtmessagesendchat").val() || '';
+        if(textmessage.length == 0){
+            alert("message sendChat not available!");
+            return;
+        }
+        myHub.server.send(textmessage);
+    })
+
+    //connect hub
+    $.connection.hub.start()
+    .done(function () {
+        console.log("SignalR Worked!");
+        WriteToPage("SignalR Worked!");
+        myHub.client.announce("Connected");
+    })
+    .fail(function () {
+        alert("Not Worked!");
+        WriteToPage("Not Worked!");
+    });
+
+    myHub.client.sendChat = function (message) {
+
+        WriteToPage("From sendChat : " +message);
+    };
+    myHub.client.announce = function (message) {
+
+        WriteToPage(message);
+    };
+
+    var WriteToPage = function (message) {
+        $("#welcome-message").append(message + "<br />");
+    }
+})()
